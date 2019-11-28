a89cbeb4003920cf76e287fcbb4e03af70edce21
MigueCas19 <38332727+MigueCas19@users.noreply.github.com>
2019-11-28 04:37:28 -0500

    Add files via upload

--- Tupla.cpp
+++ Tupla.cpp
@@ -1,87 +1,35 @@
-#include <iostream>
-using namespace std;
+/*
+ * Tupla.cpp
+ *
+ *  Created on: 23/05/2019
+ *      Author: juanferna.perez
+ */
 
-class Tupla{
-  public:
-    string curso_cod;
-    string nombre;
-    string nombre_unid;
-	int creditos;
-	int num_grupos;
 
-    Tupla(string curso_cod,string nombre,string nombre_unid,int creditos,int num_grupos){
-		this->curso_cod=curso_cod;
-		this->nombre=nombre;
-      	this->nombre_unid=nombre_unid;
-		this->creditos=creditos;
-		this->num_grupos=num_grupos;
-    }
-	Tupla(){
-		this->curso_cod="";
-		this->nombre="";
-      	this->nombre_unid="";
-		this->creditos=-1;
-		this->num_grupos=-1;
 
-	}
-	string getpk(){
-		return curso_cod;
-	}
-    void print(){
-      cout<<(curso_cod) + " | "+(nombre)+" | "+(nombre_unid)+" | "+to_string(creditos)+" | "+to_string(num_grupos)<<endl;
-    }
-		
-	void write(std::ostream& f){
+#include"Tupla.h"
+#include<iostream>
 
-		size_t size;
-	   size = curso_cod.size();
-	   f.write( (char*)&size, sizeof(size_t) );
-	   f.write( (char*)curso_cod.c_str(), size );
+Tupla::Tupla(){
+	this->id = -1;
+	this->nombre = "";
+	this->apellido = "";
+}
 
-	   size = nombre.size();
-	   f.write( (char*)&size, sizeof(size_t) );
-	   f.write( (char*)nombre.c_str(), size );
+Tupla::Tupla(int id, string nombre, string apellido){
+	this->id = id;
+	this->nombre = nombre;
+	this->apellido = apellido;
+}
 
-	   size = nombre_unid.size();
-	   f.write( (char*)&size, sizeof(size_t) );
-	   f.write( (char*)nombre_unid.c_str(), size );
+int Tupla::getID(){
+	return this->id;
+}
 
-	   f.write( (char*)&this->creditos, sizeof(int) );
+string Tupla::getNombre(){
+	return this->nombre;
+}
 
-	   f.write( (char*)&this->num_grupos, sizeof(int) );
-   }
-
-   void read(std::istream& f){
-
-		size_t size;
-	   char *data;
-	   f.read( (char*)&size, sizeof(size_t) );
-
-	   data = new char[size+1];
-	   f.read( data, size );
-	   data[size]='\0';
-	   curso_cod = data;
-	   delete data;
-
-	   f.read( (char*)&size, sizeof(size_t) );
-
-	   data = new char[size+1];
-	   f.read( data, size );
-	   data[size]='\0';
-	   nombre = data;
-	   delete data;
-
-	   f.read( (char*)&size, sizeof(size_t) );
-
-	   data = new char[size+1];
-	   f.read( data, size );
-	   data[size]='\0';
-	   nombre_unid = data;
-	   delete data;
-
-	   f.read( (char*)&this->creditos, sizeof(int) );
-
-	   f.read( (char*)&this->num_grupos, sizeof(int) );
-   }
-
-};
+string Tupla::getApellido(){
+	return this->apellido;
+}
